130f3f7b74276bc2e61161d37bc0d6f413ef9da6
donado10 <adamscoulibaly10@gmail.com>
2023-06-24 15:53:21 +0000

    adding stickyness

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector(".header-menu");
 const close_menu = document.querySelector(".close-menu");
 const navigation = document.querySelector("nav");
+const header = document.querySelector("header");
 
 menu.addEventListener("click", (e) => {
   e.preventDefault();
@@ -11,3 +12,18 @@ close_menu.addEventListener("click", (e) => {
   e.preventDefault();
   navigation.classList.toggle("open-nav");
 });
+
+const stickyMenu = function (entries) {
+  if (!entries[0].isIntersecting) {
+    menu.classList.add("sticky");
+  } else {
+    menu.classList.remove("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyMenu, {
+  root: null,
+  threshold: 0,
+});
+
+headerObserver.observe(header);
